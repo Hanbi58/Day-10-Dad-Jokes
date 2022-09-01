da3112110036f46ca9d474a62a7d70b31e1fe66a
Hanbi58 <hanxiaonan2012@gmail.com>
2022-08-31 18:46:56 -0700

    teacher's version done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//https://icanhazdadjoke.com
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+jokeBtn.addEventListener("click", generateJoke);
+
+async function generateJoke() {
+  const res = await fetch("https://icanhazdadjoke.com", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const data = await res.json();
+  jokeEl.innerText = data.joke;
+}
+// function generateJoke() {
+//   fetch("https://icanhazdadjoke.com", {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerText = data.joke;
+//     });
+// }
